b6a2c3d9aa96626d25d1c3ee1fdb9e897e39b0bd
Nishita2001 <nishita300601@gmail.com>
2020-11-17 12:50:03 +0530

    coPI and members added in model

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -3,30 +3,22 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-#import numpy as np
-#from textblob import TextBlob
-
-
-
-# Create your models here.
-'''
-analysis=TextBlob("i  hate people")
-if analysis.sentiment.polarity>0 :
-    print("positive")
-elif analysis.sentiment.polarity==0 :
-    print("neutral")
-else :
-    print("negative")
-'''
-
 
 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField()
+	coPI = models.CharField(max_length=100, blank=True, default='')
+	member1 = models.CharField(max_length=100, blank=True, default='')
+	member2 = models.CharField(max_length=100, blank=True, default='')
+	member3 = models.CharField(max_length=100, blank=True, default='')
+	member4 = models.CharField(max_length=100, blank=True, default='')
+	member5 = models.CharField(max_length=100, blank=True, default='')
+	
 	#date = models.DateTimeField(auto_now_add=True)
 	date = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
 	#sentiment = 
 	#sent = sentiment(self.content)
 	#sent = "default"
